69b02fd88f4839cd189536b2d99199018771ddeb
Bhekani2001 <philanibhekani05@gmail.com>
2024-04-25 02:30:22 +0200

    NowNehs

--- app/src/main/java/com/example/dutmaintanenceapplication/MainActivity.java
+++ app/src/main/java/com/example/dutmaintanenceapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.dutmaintanenceapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button gstart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        gstart = findViewById(R.id.getStartedButton);
+
+        gstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
