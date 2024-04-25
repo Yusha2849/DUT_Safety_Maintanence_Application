0e33068d66b5c52f3a326104273cc2b8004a31e2
Ilhaam <kaderilhaam@gmail.com>
2024-04-25 20:45:14 +0200

    updated login, main & register, added background images & user logo images

--- app/src/main/java/com/example/dutmaintanenceapplication/MainActivity.java
+++ app/src/main/java/com/example/dutmaintanenceapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.dutmaintanenceapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.main); // Replace with your root layout element's id
+
+        GradientDrawable gradientDrawable = new GradientDrawable(
+                GradientDrawable.Orientation.TOP_BOTTOM,
+                new int[]{Color.parseColor("#90CECE"), Color.parseColor("#42A5FC")});
+        gradientDrawable.setCornerRadius(0); // You can set corner radius if you want
+
+        layout.setBackground(gradientDrawable);
+
 
         gstart = findViewById(R.id.getStartedButton);
 
