a2f0257ffee4dd24dd409cf592ed012d8f199dad
Ashlee Imrith <ashleeimrith21@gmail.com>
2024-04-30 17:10:48 +0200

    Added tech home and view issue for users.

---
+++ app/src/main/java/com/example/dutmaintanenceapplication/view_issue.java
@@ -0,0 +1,24 @@
+package com.example.dutmaintanenceapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class view_issue extends AppCompatActivity {
+    //Once the student clicks the issue from the list from the fault in queue, it takes them here and they can upvote
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_view_issue);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
